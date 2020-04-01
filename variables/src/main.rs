4110e38df3f4eadf55280a2f26cc14ec43078c9d
Carlos <carlos.e.santaniello@gmail.com>
2020-04-01 16:40:13 +0200

    variables and constants

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hola, mundo. Probemos cargo run");
+    //let <nombre de la variable> = <El valor>
+    //let <nombre de la variable>: <Tipo> = <El valor>
+    //let mut <nombre de la variable> = <El valor>
+
+    /*
+    Comentario con salto de línea
+    */
+
+    const VALOR: i32 = 10;
+    let mut numero_uno = 15;
+    let numero_dos: i32 = 10;
+
+     numero_uno = 100;
+
+    let resultado = numero_uno + numero_dos + VALOR;
+
+    println!("EL resultado de ({} + {} + {}) es: {}",
+    numero_uno, numero_dos, VALOR, resultado);
 }
